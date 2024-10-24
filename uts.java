fa67ef03d45d2263fd5e138bbbf372143b27b78f
Adhityayusuf <adhitya.yusuf1206@gmail.com>
2024-10-25 01:13:40 +0700

    p

--- uts.java
+++ uts.java
@@ -2,16 +2,22 @@ import java.util.Scanner;
 public class uts {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n,m,total = 1;
+        int n,m,total = 1, j = 1;
 
-        System.out.print("Nilai n : ");
-        n = sc.nextInt();
-        System.out.print("Nilai m : ");
-        m = sc.nextInt();
+        while (j == 0) {
+             System.out.print("\nNilai n : ");
+             n = sc.nextInt();
+             System.out.print("Nilai m : ");
+             m = sc.nextInt();
 
-        for (int i = 1; i <= m; i++) {
-            total *= n;
+             if (n > 0 || m > 0) {
+                 for (int i = 1; i <= m; i++) {
+                 total *= n;
+                 }
+            } else {
+                j--;
+            }
+            System.out.println("hasil pangkat : " + total);
         }
-        System.out.println("hasil pangkat : " + total);
     }
 }
