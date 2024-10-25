2fd1c49e1ceb85ce78214043a8f4df8a778e0b67
Adhityayusuf <adhitya.yusuf1206@gmail.com>
2024-10-25 19:18:28 +0700

    P

--- uts.java
+++ uts.java
@@ -2,22 +2,21 @@ import java.util.Scanner;
 public class uts {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n,m,total = 1, j = 1;
+        int n,m,total = 1;
 
-        while (j == 0) {
+        while (true) {
+            System.out.print("=== PERHITUNGAN BERPANGKAT ===");
              System.out.print("\nNilai n : ");
              n = sc.nextInt();
              System.out.print("Nilai m : ");
              m = sc.nextInt();
 
-             if (n > 0 || m > 0) {
                  for (int i = 1; i <= m; i++) {
                  total *= n;
                  }
-            } else {
-                j--;
-            }
             System.out.println("hasil pangkat : " + total);
+            System.out.println("=== PERHITUNGAN SELESAI ===");
+            break;
         }
     }
 }
